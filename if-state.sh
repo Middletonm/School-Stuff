792798c862941daffc6a4133d9e3864ff2c939b0
Doordedoor0 <maoudismcgrath@gmail.com>
2021-09-03 19:01:43 +0100

    Added more examples to if-state.sh

--- if-state.sh
+++ if-state.sh
@@ -3,6 +3,14 @@
 if [ $1 = --help ]
 then
 	echo "--help - Displays this message"
+elif [ $1 = --version ]
+then 
+	echo "version 1.0.2"
+elif [ $1 = -i ]
+then
+	echo "Please give an input"
+	read input
+	echo "Your output is $input"
 else
 	echo "Please use args"
 fi
